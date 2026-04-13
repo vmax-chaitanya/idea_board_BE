0846c8878242990b10a3543abbcbb685855ef835
chaitanya-mindwave <Chaitanya@mindwavesolutions.com>
2026-04-14 00:51:10 +0530

    issue fixed

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,15 +1,20 @@
 const pino = require("pino");
 const { nodeEnv } = require("../config/env");
 
+// pino-pretty uses a worker transport; it fails on Vercel/serverless ("unable to determine transport target")
+const usePretty =
+  nodeEnv === "development" && !process.env.VERCEL;
+
 const logger = pino({
   level: process.env.LOG_LEVEL || "info",
-  transport:
-    nodeEnv === "development"
-      ? {
+  ...(usePretty
+    ? {
+        transport: {
           target: "pino-pretty",
           options: { colorize: true, singleLine: true },
-        }
-      : undefined,
+        },
+      }
+    : {}),
 });
 
 module.exports = logger;
